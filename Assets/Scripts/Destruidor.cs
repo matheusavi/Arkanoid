cf9be48363203246653705f400394c4aa1928af1
Matheus Avi <matheusavi.47@gmail.com>
2019-11-27 21:14:50 -0300

    UI adicionada

--- Assets/Scripts/Destruidor.cs
+++ Assets/Scripts/Destruidor.cs
@@ -1,41 +1,80 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Destruidor : MonoBehaviour
 {
     public GameObject bola;
     public Vector2 Velocidade;
+    public Text scoreText;
+    public Text lifeText;
+    public AudioSource deathSource;
+
 
     public Vector2 PosicaoInicial { get; private set; }
+
+    private int score;
+    private int lifes = 3;
+
 
     private void Start()
     {
         if (bola == null)
             bola = GameObject.FindWithTag("Bola");
         PosicaoInicial = new Vector2(bola.transform.position.x, bola.transform.position.y);
-        var rigidBody = bola.GetComponent<Rigidbody2D>();
+        updateScore();
+        updateLifes();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private async Task OnCollisionEnter2D(Collision2D collision)
     {
         var collider = collision.collider.gameObject;
 
         if (collider.name.ToLower() == "bola")
         {
-            ResetBolaPosition();
+            lifes--;
+            updateLifes();
+            if (lifes <= 0)
+                await ToggleDeathAsync();
+            else
+                ResetBolaPosition();
         }
+    }
+
+    private async Task ToggleDeathAsync()
+    {
+        deathSource.Play();
+        bola.gameObject.SetActive(false);
+        await Task.Delay(3500);
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
     }
 
     private void ResetBolaPosition()
     {
         bola.transform.position = PosicaoInicial;
-        bola.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-        bola.GetComponent<Rigidbody2D>().AddForce(Velocidade);
+        var rigidBody = bola.GetComponent<Rigidbody2D>();
+        rigidBody.velocity = new Vector2(0, 0);
+        rigidBody.AddForce(Velocidade);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        GameObject.Destroy(collision.gameObject);
+        if (collision.gameObject.CompareTag("Bloco"))
+        {
+            collision.gameObject.SetActive(false);
+            score++;
+            updateScore();
+
+        }
     }
+
+    private void updateScore()
+        => scoreText.text = $"Score: {score}";
+    private void updateLifes()
+       => lifeText.text = $"Lifes: {lifes}";
 }
